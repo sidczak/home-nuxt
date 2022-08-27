dbdb8bf0ac430f8718d63a7e2d6c4198bcec0585
sidczak <sidczak@clickmeeting.com>
2022-08-27 16:49:15 +0200

    components/Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -13,6 +13,40 @@
                     | About
                 b-nav-item(:to="localePath('pricing')")
                     | Pricing
+                b-nav-item-dropdown.dropdown-megamenu(text="MegaMenu" menu-class='dropdown-menu-megamenu')
+                    b-container(fluid)
+                        b-row.w-100(no-gutters)
+                            b-col(cols="6" lg="3")
+                                ul.list-unstyled
+                                    b-dropdown-header Glyphicons
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                ul.list-unstyled
+                                    b-dropdown-header Glyphicons
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                            b-col(cols="6" lg="3")
+                                ul.list-unstyled
+                                    b-dropdown-header Glyphicons
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                            b-col(cols="6" lg="3")
+                                ul.list-unstyled
+                                    b-dropdown-header Glyphicons
+                                    b-dropdown-item Available glyphs
+                            b-col(cols="6" lg="3")
+                                ul.list-unstyled
+                                    b-dropdown-header Glyphicons
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
+                                    b-dropdown-item Available glyphs
             b-navbar-nav(class="ml-auto")
                 b-nav-item(v-b-toggle.sidebar-settings)
                     font-awesome-icon.mr-1(icon="cog" fixed-width)
@@ -22,6 +56,9 @@
                 //-         | {{locale.name}}
                 b-nav-item(v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)")
                     | {{ locale.name }}
+                b-nav-item-dropdown(text="User" right)
+                    b-dropdown-item(href="#") Profile
+                    b-dropdown-item(href="#") Sign Out
         b-sidebar(id="sidebar-settings" title="Sidebar" :backdrop-variant="navbarColorMode.variant" :bg-variant="navbarColorMode.variant" text-variant="navbarColorMode.type" backdrop right shadow)
             div(class="px-3 py-2")
                 ColorModePicker
@@ -59,5 +96,19 @@ export default {
 <style>
 .navbar {
     transition: background-color 0.3s;
+}
+.dropdown-megamenu {
+    position: static;
+}
+.dropdown-menu-megamenu {
+    left: 0;
+    right: 0;
+    margin-top: 0;
+    border-radius: 0;
+}
+.dropdown.dropdown-hover:hover {
+    & > .dropdown-menu {
+        display: block;
+    }
 }
 </style>
