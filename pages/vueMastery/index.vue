74156fd2ff4ad13c6a1cccd58d38961f96572acb
sidczak <sidczak@clickmeeting.com>
2022-09-20 23:36:39 +0200

    add link

--- pages/vueMastery/index.vue
+++ pages/vueMastery/index.vue
@@ -3,6 +3,8 @@ Main
     template(slot='section')
         Section.h-100(:title="homeTranslation.title" :subtitle="homeTranslation.subtitle")
             template(slot='content')
+                //- NuxtLink(to="/vueMastery/socks") Socks
+                //- NuxtLink(:to="localePath('/vueMastery/socks')") Socks
 </template>
 <script>
 import Section from "../../components/common/Section";
@@ -15,8 +17,8 @@ export default {
     data() {
         return {
             homeTranslation: {
-                title: 'Vue Mastery',
-                subtitle: 'Course',
+                title: "Vue Mastery",
+                subtitle: "Course",
             },
         };
     },
