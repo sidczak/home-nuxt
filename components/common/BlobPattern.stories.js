6ebcffee70269412eb18e7902ed6c28865df53d5
sidczak <sidczak@clickmeeting.com>
2022-11-02 23:22:21 +0100

    samle BlobPattern

--- components/common/BlobPattern.stories.js
+++ components/common/BlobPattern.stories.js
@@ -3,23 +3,31 @@ import BlobPattern from "./BlobPattern";
 export default {
     title: "Common/BlobPattern",
     component: BlobPattern,
+    argTypes: {
+        pattern: {
+            control: {
+                type: "select",
+                options: ["pattern1", "pattern2", "pattern3", "waves"],
+            },
+        },
+    },
 };
 
 const Template = (args, { argTypes }) => ({
     components: { BlobPattern },
     props: Object.keys(argTypes),
     template:
-        '<BlobPattern :type="type" :shadow="shadow" :strokeWidth="strokeWidth"/>',
+        '<BlobPattern :type="type" :strokeWidth="strokeWidth" :pattern="pattern"/>',
 });
 
 export const BlobFill = Template.bind({});
 BlobFill.args = {
     type: "fill",
-    shadow: true,
 };
 
 export const BlobStroke = Template.bind({});
 BlobStroke.args = {
     type: "stroke",
     strokeWidth: 1,
+    pattern: "waves",
 };
