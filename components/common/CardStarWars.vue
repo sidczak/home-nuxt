f4cb76a1ab66dd62b5170567596bfc947ce6cf58
sidczak <sidczak@clickmeeting.com>
2023-06-22 16:47:01 +0200

    components/common/CardStarWars.stories.js

---
+++ components/common/CardStarWars.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        {{ error }}
+        <input v-model="inputValue" @input="validateInput" />
+        <p v-if="error" class="error-message">
+            Wartość powinna być między 0 a 350!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inputValue: "",
+            error: false,
+        };
+    },
+    methods: {
+        validateInput() {
+            const value = Number(this.inputValue);
+            this.error = isNaN(value) || value <= 0 || value > 350;
+        },
+    },
+};
+</script>
+
+<style>
+.error-message {
+    color: red;
+}
+</style>
